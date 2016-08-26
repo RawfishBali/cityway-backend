58566832bb4e1c29e31dce0f3cb99797caf200a5
Vitorio Santosa <vitorio.santosa@gmail.com>
2016-08-26 16:37:03 +0800

    Oh What a routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,18 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :advertisements
     resources :categories
+    resources :cities
+    resources :commonplaces
     resources :merchants
+    resources :promos
   end
+  
   get '/ping', to: proc { [200, {}, ['']] }
 
   devise_for :admins
   devise_for :users
 
   root 'home#index'
-
-  namespace :admin do
-    resources :cities
-    resources :commonplaces
-  end
 
   mount CityWay::Api::V1::Base => '/api'
 end
