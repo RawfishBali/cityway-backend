f67d0e885bd3c130e95d754f3c43743280ef7a33
Vitorio Santosa <vitorio.santosa@gmail.com>
2016-09-08 13:59:45 +0800

    merchants promo endpoints

--- app/api/city_way/api/v1/merchants.rb
+++ app/api/city_way/api/v1/merchants.rb
@@ -20,6 +20,19 @@ module CityWay
             merchant = Merchant.find(params[:id])
             present merchant, with: CityWay::Api::V1::Entities::Merchant, simple: 'false', latitude: params[:latitude], longitude: params[:longitude]
           end
+
+          desc "Merchant Detail"
+          params do
+            requires :id , type: Integer, values: -> { Merchant.ids }
+            optional :latitude, type: Float
+            optional :longitude, type: Float
+          end
+          get '/:id/promos' do
+            merchant = Merchant.find(params[:id])
+            promos = merchant.promos.page params[:page]
+            add_pagination_headers promos
+            present promos, with: CityWay::Api::V1::Entities::Promo
+          end
         end
 
       end
