bcfe64a1d5a4bf37b6706972984fec55d997d3e5
Vitorio Santosa <vitorio.santosa@gmail.com>
2016-10-07 17:38:56 +0800

    fix datepicker bug

--- app/assets/javascripts/admin/custom.js
+++ app/assets/javascripts/admin/custom.js
@@ -2,18 +2,4 @@ $(document).ready(function(){
   if($('#merchant_subcategories').length > 0){
     $('#merchant_subcategories').kendoMultiSelect();
   }
-
-  $('#subcategories')
-  .on('cocoon:after-insert', function(e, added_task) {
-    $('select').selectize()
-    $('.datepicker').each(function( index ) {
-      UIkit.datepicker($( this ), {
-        format:'YYYY-MM-DD'
-      });
-    });
-  })
-
-  UIkit.datepicker($('.datepicker'), {
-    format:'YYYY-MM-DD'
-  });
 })
