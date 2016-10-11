73d630e8724bea21d6873429aa64016def11b78b
Vitorio Santosa <vitorio.santosa@gmail.com>
2016-10-11 12:43:50 +0800

    Ugh Image Uploader

--- app/assets/javascripts/admin/custom.js
+++ app/assets/javascripts/admin/custom.js
@@ -2,17 +2,35 @@ $(document).ready(function(){
   if($('#merchant_subcategory_ids').length > 0){
     $('#merchant_subcategory_ids').kendoMultiSelect();
   }
-  $('.dropzone').html5imageupload();
+  // $('.dropzone').html5imageupload();
+
+
+  $('.dropzone').html5imageupload({
+    onSave: function() {
+      console.log('aaaaa');
+    },
+    onAfterInitImage: function() {
+		console.log('motherfucker');
+	}
+  });
 
   $('#subcategories')
   .on('cocoon:after-insert', function(e, added_task) {
     $('select').selectize()
-    $('.dropzone').html5imageupload();
+    $('.dropzone').html5imageupload({
+      onSave: function() {
+        console.log('aaaaa');
+      }
+    });
   })
 
   $('#photos')
   .on('cocoon:after-insert', function(e, added_task) {
-    $('.dropzone').html5imageupload();
+    $('.dropzone').html5imageupload({
+      onSave: function() {
+        console.log('aaaaa');
+      }
+    });
   })
 
 })
