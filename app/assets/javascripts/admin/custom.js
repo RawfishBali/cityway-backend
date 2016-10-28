18672d4db8af1ec355eba9e74bcba818b1dd54c1
Vitorio Santosa <vitorio.santosa@gmail.com>
2016-10-28 17:22:34 +0800

    checkboxes effect

--- app/assets/javascripts/admin/custom.js
+++ app/assets/javascripts/admin/custom.js
@@ -41,4 +41,25 @@ $(document).ready(function(){
     });
   })
 
+  $('#waste_pickup_schedules')
+  .on('cocoon:after-insert', function(e, added_task) {
+    var mdCheckbox = (typeof checkbox === 'undefined') ? $("[data-md-icheck],.data-md-icheck") : $(checkbox);
+    mdCheckbox.each(function() {
+      if( !$(this).next('.iCheck-helper').length ) {
+        $(this)
+        .iCheck({
+          checkboxClass: 'icheckbox_md',
+          radioClass: 'iradio_md',
+          increaseArea: '20%'
+        })
+        // validate inputs on change (parsley)
+        .on('ifChanged', function(event){
+          if ( !!$(this).data('parsley-multiple') ) {
+            $(this).parsley().validate();
+          }
+        });
+      }
+    });
+  })
+
 })
