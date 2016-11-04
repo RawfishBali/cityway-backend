8d03c485e08edbba5ce97ae919f843ae555a4c6e
Vitorio Santosa <vitorio.santosa@gmail.com>
2016-11-04 12:03:19 +0800

    some it translation

--- app/assets/javascripts/admin/parsley.js
+++ app/assets/javascripts/admin/parsley.js
@@ -2275,28 +2275,28 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
   // This is included with the Parsley library itself,
   // thus there is no use in adding it to your project.
   Parsley.addMessages('en', {
-    defaultMessage: "This value seems to be invalid.",
+    defaultMessage: "Questo valore sembra essere valido.",
     type: {
-      email: "This value should be a valid email.",
-      url: "This value should be a valid url.",
-      number: "This value should be a valid number.",
-      integer: "This value should be a valid integer.",
-      digits: "This value should be digits.",
-      alphanum: "This value should be alphanumeric."
+      email: "Questo valore deve essere un indirizzo email valido.",
+      url: "Questo valore deve essere un URL valido.",
+      number: "Questo valore deve essere un numero valido.",
+      integer: "Tale valore deve essere un numero intero valido.",
+      digits: "Tale valore deve essere cifre.",
+      alphanum: "Tale valore deve essere alfanumerico."
     },
-    notblank: "This value should not be blank.",
-    required: "This value is required.",
-    pattern: "This value seems to be invalid.",
-    min: "This value should be greater than or equal to %s.",
-    max: "This value should be lower than or equal to %s.",
-    range: "This value should be between %s and %s.",
+    notblank: "Questo valore non dovrebbe essere vuoto.",
+    required: "Questo valore è obbligatorio.",
+    pattern: "Questo valore sembra essere valido.",
+    min: "Tale valore deve essere maggiore o uguale a %s.",
+    max: "Tale valore deve essere minore o uguale a %s.",
+    range: "Tale valore deve essere compreso tra %s e %s.",
     minlength: "This value is too short. It should have %s characters or more.",
     maxlength: "This value is too long. It should have %s characters or fewer.",
-    length: "This value length is invalid. It should be between %s and %s characters long.",
+    length: "Questa lunghezza valore non è valido. Dovrebbe essere tra i caratteri %s e %s lungo.",
     mincheck: "You must select at least %s choices.",
-    maxcheck: "You must select %s choices or fewer.",
+    maxcheck: "È necessario selezionare scelte %s o meno.",
     check: "You must select between %s and %s choices.",
-    equalto: "This value should be the same."
+    equalto: "Tale valore deve essere lo stesso."
   });
 
   Parsley.setLocale('en');
