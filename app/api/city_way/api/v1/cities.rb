b029db9fd3727ea874b5d28f52bf8adbb8005447
Vitorio Santosa <vitorio.santosa@gmail.com>
2016-11-17 14:38:01 +0800

    promos and distance

--- app/api/city_way/api/v1/cities.rb
+++ app/api/city_way/api/v1/cities.rb
@@ -116,7 +116,7 @@ module CityWay
                 merchants = Merchant.where(city_id: params[:id])
               end
 
-              merchants = merchants.near([params[:latitude],params[:longitude]], 21000, units: :km) if params[:latitude] && params[:longitude]
+              # merchants = merchants.near([params[:latitude],params[:longitude]], 21000, units: :km) if params[:latitude] && params[:longitude]
 
               temp_promos = []
               merchants.each do |m|
