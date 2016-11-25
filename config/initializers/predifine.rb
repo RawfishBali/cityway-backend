dbb00db807432eafc12542267907fe3c4a0f3aa6
Vitorio Santosa <vitorio.santosa@gmail.com>
2016-11-25 15:03:11 +0800

    piscine

--- config/initializers/predifine.rb
+++ config/initializers/predifine.rb
@@ -1,2 +1,2 @@
-SPORT_DEFAULTS = ['piscine_comunali', 'campi_da_tennis', 'stadio']
+SPORT_DEFAULTS = ['piscine', 'campi_da_tennis', 'stadio']
 SCHOOL_DEFAULTS = ['scuole_materne', 'scuole_primarie', 'scuole_medie', 'scuole_secondarie', 'università', 'scuole_di_musica', 'istituti', 'istituti_professionali']
