3f6e93d0a2448e127116d7fc3bafa9c1240d1e6c
Vitorio Santosa <vitorio.santosa@gmail.com>
2017-01-09 17:59:42 +0800

    school controllers, merchant icon validation

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -44,7 +44,6 @@ class Merchant < ActiveRecord::Base
 
   validates_presence_of :name
   validates_presence_of :address
-  validates_presence_of :icon
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :save }
 
   mount_base64_uploader :icon, PhotoUploader
