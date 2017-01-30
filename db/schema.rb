9f39c60f8a006485115f1c516c3ccc85a5f15989
Vitorio Santosa <vitorio.santosa@gmail.com>
2017-01-30 12:12:45 +0800

    Sports has more than 1 phones

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170110021440) do
+ActiveRecord::Schema.define(version: 20170130035906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -611,6 +611,8 @@ ActiveRecord::Schema.define(version: 20170110021440) do
     t.integer  "utility_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "phone_1"
+    t.string   "phone_2"
   end
 
   create_table "steps", force: :cascade do |t|
