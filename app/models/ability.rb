2fa2584a2e5571f2ed83868c57c9f70497e30407
Vitorio Santosa <vitorio.santosa@gmail.com>
2017-02-10 18:42:44 +0800

    add new promo abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,7 +8,8 @@ class Ability
       can :manage, City
     elsif user.has_role? :merchant_admin
       can [:read, :update, :destroy], Merchant, admin_id: user.id
-      can :manage, Promo, id: (Promo.where('merchant_id in (?)', Merchant.where(admin_id: user.id).map(&:id))).map(&:id)
+      can :create, Promo
+      can [:read, :update, :destroy], Promo, id: (Promo.where('merchant_id in (?)', Merchant.where(admin_id: user.id).map(&:id))).map(&:id)
     end
   end
 end
